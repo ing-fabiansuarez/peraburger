87bafda66c9a603d66bfe890ebccab6a6531889a
fsuarez120-fabian <fsuarez120@unab.edu.co>
2021-06-17 09:40:52 -0500

    Modificacion en la base de datos 2021-06-17

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'https://localhost/peraburger';
+	public $baseURL = 'http://localhost/peraburger/';
 
 	/*
 	|--------------------------------------------------------------------------
