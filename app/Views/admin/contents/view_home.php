763d34ca98aecf92abc97f1e0e918e8f2dc659e1
fsuarez120-fabian <fsuarez120@unab.edu.co>
2021-07-15 14:28:53 -0500

    Modificacion del home

--- app/Views/admin/contents/view_home.php
+++ app/Views/admin/contents/view_home.php
@@ -7,17 +7,17 @@
 
 <section class="content">
     <div class="container-fluid">
-
+        <br>
         <div class="row">
             <div class="col-md-3">
             </div>
             <div class="col-md-6">
-                <img src="<?= base_url() ?>/public/img/peraburgelogohome.png" alt="Logo" class="img-fluid" style="width: 100%;">
+                <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2FAlpargatasPeRa%2Fposts%2F3280066705553839&show_text=true&width=500" width="100%" height="647" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
             </div>
             <div class="col-md-3">
+            </div>
         </div>
-        </div>
-        
+
     </div>
 </section>
 
